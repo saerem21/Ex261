4aaf43af37bd00dba082132275dce5431df43313
saerem21 <nemessisdds>
2014-01-08 13:41:50 +0900

    웹뷰 입니다.
    에러는 없지만 작동은 안됨
    page261

--- Ex261/ViewController.m
+++ Ex261/ViewController.m
@@ -8,11 +8,42 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate,UIWebViewDelegate>
+@property (weak , nonatomic)IBOutlet UIWebView *webView;
+@property (weak , nonatomic)IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
+
+-(BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType{
+    if(UIWebViewNavigationTypeLinkClicked == navigationType){
+        NSLog(@"링크 클릭 금지");
+        return  NO;
+    }
+    return  YES;
+}
+
+-(void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
+    NSString *msg = [error localizedDescription];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"에러" message:msg delegate:Nil cancelButtonTitle:@"확인" otherButtonTitles: nil];
+    [alert show];
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self go:Nil];
+    return YES;
+}
+
+-(IBAction)go:(id)sender{
+    if([_textField.text length] > 3){
+        NSURL *url = [NSURL URLWithString:_textField.text];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        [_webView loadRequest:request];
+        [_textField resignFirstResponder];
+    }
+}
 
 - (void)viewDidLoad
 {
